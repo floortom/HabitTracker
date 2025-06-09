e4e9792e79a7caba8fd27010b9f44712b4deb575
PLFLORCZUKT <Tomasz.Florczuk@tetrapak.com>
2025-06-09 13:07:48 +0200

    Added display and save habit features

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
+habits = ["Test habit", "Test habit2"]
 
 @app.route("/")
 def index():
-    return render_template("index.html", title="Habit Tracker - Home")
+    return render_template("index.html",
+                           title="Habit Tracker - Home",
+                           habits=habits
+                           )
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add_habit():
+    if request.method == "POST":
+        habit = request.form.get("habit")
+        habits.append(habit)
     return render_template("addHabit.html", title="Habit Tracker - Add Habit")
