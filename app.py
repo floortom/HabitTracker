51b9e016743bc1af27ead190dcd58c20f6494e69
PLFLORCZUKT <Tomasz.Florczuk@tetrapak.com>
2025-06-10 09:53:32 +0200

    Added date navigation feature

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
+import datetime
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 habits = []
 
+@app.context_processor
+def add_calc_date_range():
+    def date_range(start: datetime.date):
+        dates = [start + datetime.timedelta(diff) for diff in range(-3, 4)]
+        return dates
+    return {"dateRange": date_range}
+
 @app.route("/")
 def index():
+    dateStr = request.args.get("date")
+    if dateStr:
+        selectedDate = datetime.date.fromisoformat(dateStr)
+    else:
+        selectedDate = datetime.date.today()
     return render_template("index.html",
                            title="Habit Tracker - Home",
-                           habits=habits
+                           habits=habits,
+                           selectedDate=selectedDate
                            )
 
 
@@ -16,4 +30,7 @@ def add_habit():
     if request.method == "POST":
         habit = request.form.get("habit")
         habits.append(habit)
-    return render_template("addHabit.html", title="Habit Tracker - Add Habit")
+    return render_template("addHabit.html",
+                           title="Habit Tracker - Add Habit",
+                           selectedDate=datetime.date.today()
+                           )
